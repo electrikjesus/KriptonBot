1e5c305756b25bac5c9f334ff1e47656eee516e0
Özcan Esen <ozcanesen@gmail.com>
2015-12-16 10:45:33 +0200

    A Telegram bot that follows Libreoffice's Gerrit updates, and sends message when a kripton person sends a new patch.

---
+++ KriptonGerritBot.py
@@ -0,0 +1,50 @@
+import json
+import urllib2
+import os
+import time
+import telegram
+
+kripton_people = [
+    'kadertarlan', 
+    'yeliztaneroglu', 
+    'ozcanesen', 
+    'aybuke', 
+    'gulsahkose'
+    ]
+
+bot = telegram.Bot(token='<<TOKEN>>')
+
+while True:
+    data = urllib2.urlopen('http://gerrit.libreoffice.org/changes/?n=25&O=81')
+    json_text = data.read()
+    json_text = json_text[5:] # trim
+
+    latest_changes = json.loads(json_text)
+
+    for patch in latest_changes:
+
+        _number = str(patch['_number'])
+
+        if patch['owner']['username'] in kripton_people and not os.path.exists("sentcommits/" + _number):
+
+                message = 'Gonderen: ' + patch['owner']['username'] + \
+                        '\n - Durum: ' + patch['status'] + \
+                        '\n - Baslik: ' + patch['subject'] + \
+                        '\n - Degisiklik: +' + str(patch['insertions']) + ' -' + str(patch['deletions']) + \
+                        '\n - Baglanti: https://gerrit.libreoffice.org/#/c/' + _number
+
+                subscribers = []
+                for update in bot.getUpdates():
+                    chat_id = update['message']['chat']['id']
+                    if chat_id not in subscribers:
+                        subscribers.append(chat_id)
+
+                for chat_id in subscribers:
+                    try:
+                        bot.sendMessage(chat_id, message)
+                    except:
+                        pass
+
+                open("sentcommits/" + _number, 'a').close() # touch sentcommits/id
+
+    time.sleep(10)
